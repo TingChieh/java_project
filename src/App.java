b2b130b0c3a9fc402cee5840309ec8af0b87b7de
TingChieh <tingchieh77@outlook.com>
2022-11-10 23:41:40 +0800

    test1vscode

--- src/App.java
+++ src/App.java
@@ -3,10 +3,10 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-            int a = input.nextInt();
-            int b = input.nextInt();
-            int c = a*b;
-            if (c==888) {
+            int wsc = input.nextInt();
+            int wcg = input.nextInt();
+            int abc = wsc*wcg;
+            if (abc==888) {
                 System.out.println("该四边形为发财矩形");
             } else {
                 System.out.println("普通矩形");
